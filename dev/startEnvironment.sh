ac9aac2f6dcc29a3c3616fb8a022581190d6bc39
trevSmart <203040921+trevSmart@users.noreply.github.com>
2025-09-21 03:50:03 +0200

    Se actualiza la dependencia de @salesforce/cli a la versión 2.106.7 en las configuraciones de package.json y package-lock.json. Además, se realizan mejoras en la gestión de errores del servidor MCP, optimizando la lógica de respuesta y ajustando las pruebas para reflejar estos cambios.

---
+++ dev/startEnvironment.sh
@@ -0,0 +1,20 @@
+source .env
+
+npm install @salesforce/cli -g && npm install
+
+response=$(curl -s -X POST "https://test.salesforce.com/services/oauth2/token" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "grant_type=password" \
+  -d "client_id=$SF_ORG_CLIENT_ID" \
+  -d "client_secret=$SF_ORG_CLIENT_SECRET" \
+  -d "username=$SF_ORG_CLIENT_USERNAME" \
+  -d "password=$SF_ORG_CLIENT_PASSWORD")
+
+apt install jq
+
+echo "$response" | jq -r '.access_token'
+echo "$response" | jq -r '.instance_url'
+
+sf org login access-token --instance-url "$SF_INSTANCE_URL" --no-prompt --alias "$SF_ORG_ALIAS" --set-default
+
+sf org display
